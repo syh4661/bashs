32f9b07f701369c80cacce3298ecedb41438863e
YongHyeok Seo <syh20131292@gmail.com>
2022-02-16 17:45:13 +0900

    Create joint_monitor.sh

---
+++ joint_monitor.sh
@@ -0,0 +1,2 @@
+# 2560x1440 rate 99.95, make two monitor joint display 
+xrandr --output HDMI-0 --mode 2560x1440 --pos 2560x0 --rotate normal -r 99.95 --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal -r 99.95
